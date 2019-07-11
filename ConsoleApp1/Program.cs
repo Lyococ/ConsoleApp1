20d9aa56da385b5d61e0f4122e44ad52c02f9363
Diego Armando Reyes Neira <diego.reyes@evertecinc.com>
2019-07-11 14:19:29 -0500

    ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlfffd!");
+            Console.WriteLine("Hello Worlfffdsdfg!");
 
         }
     }
